2552180f8af7b820ce5cb3768e24475fbaf8da03
Jared Good <jared_danell@yahoo.com>
2021-11-08 10:14:16 -0700

    add styles and classes dynamically

--- hello-world/src/app/server/server.component.ts
+++ hello-world/src/app/server/server.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 
+
 @Component({
 	selector: 'app-server', //html component that will use this class
 	templateUrl: 'server.component.html', //template that the will be swapped for the above selector
   styleUrls: ['server.component.css']
 })
 
+// @ts-ignore
 export class ServerComponent {
   serverId: number = Math.floor(Math.random() * 100000) + 1;
-  serverStatus: string = (Math.random() >= 0.5) ? "Online" : "Offline";
+  serverStatus: string = "Offline"
+
+  constructor() {
+    this.serverStatus = (Math.random() >= 0.5) ? "Online" : this.serverStatus;
+  }
+  getColor(){
+    return (this.serverStatus) === 'Online' ? 'seagreen' : 'red'
+  }
 }
