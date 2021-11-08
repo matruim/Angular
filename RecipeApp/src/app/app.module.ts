68919bdfad3ef7c1d0deaa8f14c28776a672bc3c
Jared Good <jared_danell@yahoo.com>
2021-11-08 11:24:15 -0700

    add base components

--- RecipeApp/src/app/app.module.ts
+++ RecipeApp/src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { ShoppingListComponent } from './component/shopping/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './component/shopping/shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { RecipesComponent } from "./component/recipes/recipes.component";
+import { RecipeListComponent } from './component/recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './component/recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './component/recipes/recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent
   ],
   imports: [
     BrowserModule
