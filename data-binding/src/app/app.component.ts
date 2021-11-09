07b01ebe585988ca8cb131dd66f65b80da60ac6a
Jared Good <jared_danell@yahoo.com>
2021-11-09 14:59:55 -0700

    custom data binding

--- data-binding/src/app/app.component.ts
+++ data-binding/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ServerElementComponent} from "./server-element/server-element.component";
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [];
+  serverElements = [{type:'server',name:'Testserver',content:'just a test'}];
 
-
+  onServerAdded(serverData: {name:string, content: string}) {
+      this.serverElements.push({
+        type: 'server',
+        name: serverData.name,
+        content: serverData.content
+      });
+  }
+  //
+  onBlueprintAdded(blueprintData: {name:string, content: string}) {
+      this.serverElements.push({
+        type: 'blueprint',
+        name: blueprintData.name,
+        content: blueprintData.content
+      });
+  }
 }
