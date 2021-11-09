c4c4014eb49f0f5d655f91ef58bde31f4220159d
Jared Good <jared_danell@yahoo.com>
2021-11-09 08:33:54 -0700

    add recipe framework

--- RecipeApp/src/app/component/recipes/recipe-list/recipe-list.component.ts
+++ RecipeApp/src/app/component/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipes.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("Mac and Cheese","noodles in a cheese sauce","https://media.istockphoto.com/photos/baked-mac-and-cheese-picture-id1194753924?k=20&m=1194753924&s=612x612&w=0&h=sGdrke6pXyMKBI4YXoAt3ShD99RufnGcdcvU1503hhI="),
+    new Recipe("Spaghetti", "noodles in a tomate sauce", "")
+  ];
 
   constructor() { }
 
