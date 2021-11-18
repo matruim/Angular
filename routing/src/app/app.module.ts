e5fdb7b4cbf050eb7ff172e4728ca6906579a3ad
Jared Good <jared_danell@yahoo.com>
2021-11-18 13:34:15 -0700

    routing examples

--- routing/src/app/app.module.ts
+++ routing/src/app/app.module.ts
@@ -9,14 +9,9 @@ import {UserComponent} from './users/user/user.component';
 import {EditServerComponent} from './servers/edit-server/edit-server.component';
 import {ServerComponent} from './servers/server/server.component';
 import {ServersService} from './servers/servers.service';
-import {RouterModule, Routes} from '@angular/router';
-
-const appRoutes: Routes = [
-  {path: '',component: HomeComponent},
-  {path: 'users',component: UsersComponent},
-  {path: 'servers',component: ServersComponent}
-]
-
+import { NotFoundComponent } from './not-found/not-found.component';
+import {AppRoutingModule} from "./app-routing.module";
+import {CanDeactivateGuard} from "./servers/edit-server/can-deactivate-guard.service";
 
 @NgModule({
   declarations: [
@@ -26,14 +21,15 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
-  providers: [ServersService],
+  providers: [ServersService, CanDeactivateGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule {
