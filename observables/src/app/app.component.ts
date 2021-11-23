559ef1ef5a9943169bd902e7b9cc79b8fe7f6cd2
Jared Good <jared_danell@yahoo.com>
2021-11-23 13:53:47 -0700

    finsihed with obserables and Subjects

--- observables/src/app/app.component.ts
+++ observables/src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {UserService} from "./user/user.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
+  userActivated = false;
+  constructor(private userService: UserService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.userService.activatedEmitter.subscribe( didActivate => {
+      this.userActivated = didActivate;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.userService.activatedEmitter.unsubscribe();
+  }
 }
--- observables/src/app/home/home.component.ts
+++ observables/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {interval, Observable, Subscription} from 'rxjs';
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  private firstObsSubscription: Subscription;
+  private secondObsSubscription: Subscription;
   constructor() { }
 
   ngOnInit() {
+    // this.firstObsSubscription = interval(1000).subscribe(count => {
+    //   console.log(count);
+    // });
+    const customIntervalObservable = new Observable(observer =>{
+      let count = 0;
+      setInterval(() =>{
+        observer.next(count);
+        if(count == 2){
+          observer.complete();
+        }
+        if(count > 3){
+          observer.error(new Error('Count is greater than 3.'))
+        }
+        count++;
+      }, 1000);
+    });
+
+    this.secondObsSubscription = customIntervalObservable.pipe(map((data:number) => `Round: ${data+1}`)).subscribe(data => {
+      console.log(data);
+    }, error => {
+      alert(error.message);
+    }, () =>{
+      console.log('completed');
+    });
+  }
+  ngOnDestroy(){
+    // this.firstObsSubscription.unsubscribe();
+    this.secondObsSubscription.unsubscribe();
   }
 
 }
